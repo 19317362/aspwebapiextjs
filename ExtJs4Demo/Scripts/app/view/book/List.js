b04d62e2451a4a5ac26e958cdbf8f78e81f19fd7
Wendell Wayne Estrada <whizkidwwe1217@live.com>
2016-02-25 12:11:24 +0800

    Add css

---
+++ ExtJs4Demo/Scripts/app/view/book/List.js
@@ -0,0 +1,19 @@
+﻿Ext.define('ExtJs4Demo.view.book.List', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.bookList',
+
+    title: 'All Books',
+
+    initComponent: function () {
+        this.store = 'Books',
+
+        this.columns = [
+            { header: 'Title', dataIndex: 'Title', flex: 1 },
+            { header: 'Author', dataIndex: 'AuthorName', flex: 1 },
+            { header: 'Year', dataIndex: 'Year', flex: 1 },
+            { header: 'Genre', dataIndex: 'Genre', flex: 1 },
+            { header: 'Price', dataIndex: 'Price', flex: 1 }
+        ];
+        this.callParent(arguments);
+    }
+});
